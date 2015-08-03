f16b90f41ec7f1d1a7854d4c8b64c9b473e811ad
rafael <neopytek@gmail.com>
2015-08-03 19:51:42 +0200

    Añado lo necesario para Borrar pregunta

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
@@ -12,13 +12,13 @@ router.get('/', function(req, res) {
 router.param('quizId', quizController.load);  // autoload :quizId
 
 // Definición de rutas de /quizes
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
-router.get('/author',       quizController.author);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
 module.exports = router;
