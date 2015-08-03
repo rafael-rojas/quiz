269cebc471a6d23cba2355b4a11b60218ceade68
rafael <neopytek@gmail.com>
2015-08-03 19:15:20 +0200

    Añado validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,37 +1,70 @@
-// GET /quizes/question
-exports.question = function(req, res) {
-   res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+
+// Autoload :id
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else{next(new Error('No existe quizId=' + quizId))}
+    }
+  ).catch(function(error){next(error)});
 };
 
-// GET /quizes/answer
+// GET /quizes
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+    }
+  ).catch(function(error){next(error)});
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-   if (req.query.respuesta === 'Roma'){
-      res.render('quizes/answer', {respuesta: 'Correcto'});
-   } else {
-      res.render('quizes/answer', {respuesta: 'Incorrecto'});
-   }
-};
-
-
-exports.author = function(req, res) {
-   res.render('author', {autor: 'Rafael Rojas',imagen: '/images/autor.jpeg', title: 'Quiz'});
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render(
+    'quizes/answer', 
+    { quiz: req.quiz, 
+      respuesta: resultado, 
+      errors: []
+    }
+  );
 };
 
 // GET /quizes/new
 exports.new = function(req, res) {
-  var quiz = models.Quiz.build(
+  var quiz = models.Quiz.build( // crea objeto quiz 
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');  
-  })   // res.redirect: Redirección HTTP a lista de preguntas
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
 };
---
+++ models/quiz.js
@@ -0,0 +1,17 @@
+ 
+// Definicion del modelo de Quiz con validación
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+  	'Quiz',
+    { pregunta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+      },
+      respuesta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+      }
+    }
+  );
+}
